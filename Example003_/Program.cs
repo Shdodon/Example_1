8ad287cfebdd33bbbb46f00772dc49603a62e55f
Pavel Danilov <danilov211@mail.ru>
2023-01-25 22:43:23 +0300

    add Random

--- Example003_/Program.cs
+++ Example003_/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA=12;
-double numberB=5;
+﻿double numberA=new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+double numberB=new Random().Next(2,5); // 2 3 4
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA/numberB);
